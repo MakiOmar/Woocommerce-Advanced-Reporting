5dad1ac29d800a0148780d123e7cd3cf4767c2d1
TaskFlow Developer <developer@taskflow.app>
2025-10-31 13:51:51 +0200

    Fix str_replace null parameter deprecation in fetch_data_details

--- includes/fetch_data_details.php
+++ includes/fetch_data_details.php
@@ -51,7 +51,7 @@ if ($file_used == "sql_table") {
     $pw_order_status   = $this->pw_get_woo_requests('pw_orders_status', '-1', true);
     //$pw_order_status  		= "'".str_replace(",","','",$pw_order_status)."'";
 
-    $pw_paid_customer = str_replace(",", "','", $pw_paid_customer);
+    $pw_paid_customer = str_replace(",", "','", (string)$pw_paid_customer);
 
     $pw_coupon_code  = $this->pw_get_woo_requests('coupon_code', '-1', true);
     $pw_coupon_codes = $this->pw_get_woo_requests('pw_codes_of_coupon', '-1', true);
